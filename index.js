792e13369b7da4afb822b6444bf6758f759596b9
Nazeerkhan1234 <nazeerkhan9695@gmail.com>
2023-08-30 17:46:37 +0530

    file upload

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("hi")
+const express = require("express");
+const app = express();
+const multer = require("multer");
+app.use(express.json());
+require("dotenv").config();
+//How to upload a file + text in Node/Express
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './upload')
+    },
+    filename: function (req, file, cb) {
+        const random=Math.ceil(Math.random()*100);
+    let filename=random+""+file.originalname;
+    cb(null,filename)
+    }
+  })
+  const upload = multer({ storage: storage })
+
+app.post('/upload',upload.single('file'),(req,res)=>{
+    console.log(req.body.surname)
+    res.status(200).json({msg:"file upload successfully"})
+})
+
+let port = process.env.port;
+app.listen(port, () => {
+  console.log("the server is running on the port  " + port);
+});
