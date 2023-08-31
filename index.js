e1853096678421008544ea8de179c5a918e4c617
Nazeerkhan1234 <nazeerkhan9695@gmail.com>
2023-08-31 09:43:55 +0530

    file upload

--- index.js
+++ index.js
@@ -1,25 +1,38 @@
+
 const express = require("express");
 const app = express();
 const multer = require("multer");
+const cloudinary = require('cloudinary');
 app.use(express.json());
 require("dotenv").config();
 //How to upload a file + text in Node/Express
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, './upload')
-    },
-    filename: function (req, file, cb) {
-        const random=Math.ceil(Math.random()*100);
-    let filename=random+""+file.originalname;
-    cb(null,filename)
-    }
-  })
-  const upload = multer({ storage: storage })
+  destination: function (req, file, cb) {
+    cb(null, "./upload");
+  },
+  filename: function (req, file, cb) {
+    const random = Math.ceil(Math.random() * 100);
+    let filename = random + "" + file.originalname;
+    cb(null, filename);
+  },
+});
+const upload = multer({ storage: storage });
+// How to upload a file in cloudinary
+cloudinary.config({
+  cloud_name: "anyway",
+  api_key: "395862488124532",
+  api_secret: "APqfiY3sutO6kPanwr1NMbDAkuo",
+});
 
-app.post('/upload',upload.single('file'),(req,res)=>{
-    console.log(req.body.surname)
-    res.status(200).json({msg:"file upload successfully"})
-})
+app.post("/upload", upload.single("file"), (req, res) => {
+  // console.log(req.body.name)
+  cloudinary.uploader.upload(req.file.path,
+    function (error, result) {
+      console.log(error,result);
+    }
+  );
+  res.status(200).json({ msg: "file upload successfully" });
+});
 
 let port = process.env.port;
 app.listen(port, () => {
